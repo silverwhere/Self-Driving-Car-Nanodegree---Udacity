5ad033e4cd883c11002473731377aabd5c0b0d07
Ian <48192533+silverwhere@users.noreply.github.com>
2021-04-07 22:11:00 -0400

    Update FusionEKF.cpp

--- Project 5 - Extended Kalman Filter/src/FusionEKF.cpp
+++ Project 5 - Extended Kalman Filter/src/FusionEKF.cpp
@@ -140,6 +140,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float noise_ay = 9;
 
  // process covariance Q matrix
+  ekf_.Q_ = MatrixXd(4, 4);
   ekf_.Q_ << dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
              0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
              dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
