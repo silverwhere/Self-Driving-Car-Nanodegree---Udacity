0fa852c8c5ca6b9dbe0904db7db20852a0a787bb
Ian <48192533+silverwhere@users.noreply.github.com>
2021-05-21 22:34:37 -0400

    Add files via upload

---
+++ Project 8 - PID Controller/build/CMakeFiles/pid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pid.dir/src/PID.cpp.o"
+  "CMakeFiles/pid.dir/src/main.cpp.o"
+  "pid.pdb"
+  "pid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
